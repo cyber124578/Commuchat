1612522b609ed169bbc317f44ee4e30946322a1b
firas <firaswebdev@protonmail.com>
2020-06-22 17:36:50 +0100

    Gallery prompt

--- app/src/main/java/com/example/commuchat/Activities/RegisterActivity.java
+++ app/src/main/java/com/example/commuchat/Activities/RegisterActivity.java
@@ -1,4 +1,89 @@
 package com.example.commuchat.Activities;
 
-public class RegisterActivity {
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.example.commuchat.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    ImageView ImgUserPhoto;
+    static  int PReqCode = 1 ;
+    static  int REQUESCODE = 1;
+    Uri pickedImgUri;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        //view
+        ImgUserPhoto = findViewById(R.id.regUserPhoto);
+
+        ImgUserPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (Build.VERSION.SDK_INT>=22) {
+                    checkAndRequestForpermission();
+                }
+                else
+                {
+                    openGallery();
+                }
+            }
+        });
+    }
+
+    private void openGallery() {
+        //ouvrir: le gallery et attends l'utilisateur pour choisir un photo
+        Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("image/*");
+        startActivityForResult(galleryIntent,REQUESCODE);
+
+    }
+
+    private void checkAndRequestForpermission() {
+        if (ContextCompat.checkSelfPermission(RegisterActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)
+            != PackageManager.PERMISSION_GRANTED)
+            if (ActivityCompat.shouldShowRequestPermissionRationale(RegisterActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                Toast.makeText(RegisterActivity.this, "Please accept for required permissions", Toast.LENGTH_SHORT).show();
+            }
+        else {
+                ActivityCompat.requestPermissions(RegisterActivity.this,
+                                                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                                                    PReqCode);
+            }
+        else
+        {
+            openGallery();
+        }
+
+
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK && requestCode == REQUESCODE && data != null){
+
+            // l'utilisateur a choisi une image avec succès
+            // nous devons enregistrer sa référence à une variable Uri
+            pickedImgUri = data.getData();
+            ImgUserPhoto.setImageURI(pickedImgUri);
+        }
+    }
+
+
 }
